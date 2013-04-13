24a343b77018d31154ae674dce2d06d6e1699123
gizmoabhinav <gizmoabhinav@gmail.com>
2013-04-13 23:23:33 +0530

    Create router.js

---
+++ router.js
@@ -0,0 +1,31 @@
+function route(pathname) {
+  console.log("About to route a request for " + pathname);
+  var mysql = require('mysql');
+var str=pathname.substring(1);
+var result;
+var connection = mysql.createConnection({
+host : 'localhost',
+user : 'root',
+password : 'bazinga420',
+});
+
+connection.connect();
+
+console.log("connected.");
+connection.query('use openmrs', function(err, rows, fields) {
+if (err) throw err;
+
+//console.log('Query result: ', rows);
+});
+connection.query('SELECT person_name.given_name, person.gender, person_address.address1 FROM person, person_name, person_address WHERE person.person_id=person_name.person_id AND person.person_id=person_address.person_id AND person_name.given_name like \'%'+str+'%\'', function(err, rows, fields) {
+if (err) throw err;
+
+console.log('Query result: ', rows);
+});
+
+
+connection.end();
+
+}
+
+exports.route = route;
