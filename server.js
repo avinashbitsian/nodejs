a59a1d868e662eebfc616e3136efaebc4fecdaa7
gizmoabhinav <gizmoabhinav@gmail.com>
2013-04-17 12:41:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 var http = require("http");
 var url = require("url");
-
+var output;
 function start(route) {
   function onRequest(request, response) {
+    var start = +new Date();
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
-
-    route(pathname);
+  
+	var username=fetchauth();
+    route(pathname,username);
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
+    //response.write("Hello World");
+	var end = +new Date();
+	response.write("Done in " + (end-start) + " miliseconds");
     response.end();
   }
 
   http.createServer(onRequest).listen(8888);
   console.log("Server has started.");
+   
 }
 
 exports.start = start;
