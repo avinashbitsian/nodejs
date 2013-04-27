a1db99cab3a10f4b104a99219c617ea3462d03e2
gizmoabhinav <gizmoabhinav@gmail.com>
2013-04-27 10:45:46 +0530

    Update router.js

--- router.js
+++ router.js
@@ -1,34 +1,58 @@
-
-function route(pathname,username) {
-  console.log("About to route a request for " + pathname);
-
+function route(values,username,pass) {
+console.log("About to route a request for " + values);
 var mysql = require('mysql');
-var str=pathname.substring(1);
-var result;
+var crypto = require('crypto');
+var str=values;
+var hashed='';
+var authenticated= new Boolean();
+
 var connection = mysql.createConnection({
 host : 'localhost',
-user : username,
+user : 'root',
 password : 'bazinga420',
 });
 
 connection.connect();
 
 console.log("connected.");
+
+
 connection.query('use openmrs', function(err, rows, fields) {
 if (err) throw err;
 
 //console.log('Query result: ', rows);
 });
-connection.query('SELECT person_name.given_name, person.gender, person_address.address1 FROM person, person_name, person_address WHERE person.person_id=person_name.person_id AND person.person_id=person_address.person_id AND person_name.given_name like \'%'+str+'%\'', function(err, rows, fields) {
+
+
+connection.query('SELECT salt FROM users WHERE system_id = \''+username+'\'', function(err, rows, fields) {
 if (err) throw err;
-
-console.log('Query result: ', rows);
-
+pass+=JSON.stringify(rows).substring(10,JSON.stringify(rows).length-3);
+console.log('password is ' + pass);
+hashed += crypto.createHash('sha512').update(pass).digest('hex');
+console.log('hashed password is ' +hashed);
 });
 
 
-connection.end();
 
+connection.query('SELECT password FROM users WHERE system_id = \''+username+'\'', function(err, rows, fields) {
+  if (err) throw err;
+//	console.log('database password is ' + JSON.stringify(rows).substring(14,JSON.stringify(rows).length-3));
+	if(JSON.stringify(rows).substring(14,JSON.stringify(rows).length-3)==hashed){
+		authenticated=true;
+	}
+});
+
+if (authenticated){
+	connection.query('SELECT person_name.given_name, person.gender, person_address.address1 FROM person, person_name, person_address WHERE person.person_id=person_name.person_id AND person.person_id=person_address.person_id AND person_name.given_name like \'%'+str+'%\'', function(err, rows, fields) {
+	if (err) throw err;
+
+	console.log('Query result: ', rows);
+
+	});
+
+
+	connection.end();
+}
 }
 
 exports.route = route;
