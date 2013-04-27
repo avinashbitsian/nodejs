2044247c667001c2bb60a20870ffb5906c390e8f
gizmoabhinav <gizmoabhinav@gmail.com>
2013-04-27 10:46:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,42 @@
 var http = require("http");
 var url = require("url");
-var output;
+var fs = require('fs');
+path = require('path');
 function start(route) {
   function onRequest(request, response) {
     var start = +new Date();
-    var pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + " received.");
-  
-	var username=fetchauth();
-    route(pathname,username);
+	var data = '';
+      
+       request.on('data', function (chunk) {
+          data += chunk;
+       });
+      
+       request.on('end', function () {
+        
+          var values = JSON.parse(data);
+          var username = values.split(" ")[1].substring(1,values.split(" ")[1].length-1);
+		  var pass = values.split(" ")[2].substring(1,values.split(" ")[2].length-1);
+		  values = values.split(" ")[0];
+          console.log("Request for " + values + " received.");
+		  console.log("username " + username);
+		  console.log("password " + pass);
+		  route(values,username,pass);
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
+       });
+	
+	
+	
+    //var pathname = url.parse(request.url).pathname;
+    
+    //response.writeHead(200, {"Content-Type": "text/plain"});
     //response.write("Hello World");
 	var end = +new Date();
-	response.write("Done in " + (end-start) + " miliseconds");
+	//response.write(" Done in " + (end-start) + " miliseconds \n");
     response.end();
   }
+ 
 
-  http.createServer(onRequest).listen(8888);
+  http.createServer(onRequest).listen(8585);
   console.log("Server has started.");
    
 }
